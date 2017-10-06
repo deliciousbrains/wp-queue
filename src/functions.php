e612dc371e4ca3574c4c7e926336765e0bb898d3
Ashley Rich <hello@ashleyrich.com>
2017-10-06 18:41:29 +0100

    First pass as readme

--- src/functions.php
+++ src/functions.php
@@ -15,3 +15,39 @@ if ( ! function_exists( 'wp_queue' ) ) {
 		return QueueManager::resolve( $connection );
 	}
 }
+
+if ( ! function_exists( 'wp_queue_install_tables' ) ) {
+	/**
+	 * Install database tables
+	 */
+	function wp_queue_install_tables() {
+		global $wpdb;
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+		$wpdb->hide_errors();
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE {$wpdb->prefix}queue_jobs (
+				id bigint(20) NOT NULL AUTO_INCREMENT,
+				job longtext NOT NULL,
+				attempts tinyint(3) NOT NULL DEFAULT 0,
+				reserved_at datetime DEFAULT NULL,
+				available_at datetime NOT NULL,
+				created_at datetime NOT NULL,
+				PRIMARY KEY  (id)
+				) $charset_collate;";
+
+		dbDelta( $sql );
+
+		$sql = "CREATE TABLE {$wpdb->prefix}queue_failures (
+				id bigint(20) NOT NULL AUTO_INCREMENT,
+				job longtext NOT NULL,
+				error text DEFAULT NULL,
+				failed_at datetime NOT NULL,
+				PRIMARY KEY  (id)
+				) $charset_collate;";
+
+		dbDelta( $sql );
+	}
+}
