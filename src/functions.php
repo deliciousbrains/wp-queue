8e3e29b47f0c024959af076a405f5668951bfe92
Ashley Rich <hello@ashleyrich.com>
2017-10-06 16:05:48 +0100

    Introduce a QueueManager class

--- src/functions.php
+++ src/functions.php
@@ -1,22 +1,17 @@
 <?php
 
 use WP_Queue\Queue;
+use WP_Queue\QueueManager;
 
 if ( ! function_exists( 'wp_queue' ) ) {
 	/**
 	 * Return Queue instance.
 	 *
+	 * @param string $connection
+	 *
 	 * @return Queue
 	 */
-	function wp_queue() {
-		static $queue = null;
-
-		if ( is_null( $queue ) ) {
-			global $wpdb;
-			$connection = apply_filters( 'wp_queue_connection', new WP_Queue\Connections\DatabaseConnection( $wpdb ) );
-			$queue      = new WP_Queue\Queue( $connection );
-		}
-
-		return $queue;
+	function wp_queue( $connection = 'database' ) {
+		return QueueManager::resolve( $connection );
 	}
 }
